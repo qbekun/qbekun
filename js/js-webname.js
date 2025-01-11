2098536a8a20935623610a40b1777721c5dece04
qbekun <73672498+qbekun@users.noreply.github.com>
2025-01-11 15:53:21 +0100

    Update js-webname.js

--- js/js-webname.js
+++ js/js-webname.js
@@ -1,58 +1,71 @@
-if (document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", function () {
-        loaded();
-    });
-} else if (document.attachEvent) {
-    document.attachEvent("onreadystatechange", function () {
-        loaded();
-    });
-}
+document.addEventListener("DOMContentLoaded", function () {
+    loaded();
+});
 
 function loaded() {
-
     setInterval(loop, 300);
-
 }
 
 var x = 0;
-
-var titleText = ["|",
-		"$",
-		"$ q_",
-		"$ qb_",
-		"$ qbe_",
-		"$ qbek_",
-		"$ qbeku_",
-		"$ qbekun_",
-                "$ qbekun_",
-                "$ qbekun_",
-                "$ qbekun|",
-                "$ qbeku|",
-                "$ qbek|",
-                "$ qbe|",
-                "$ qb|",
-                "$ q|",
-                " ", ];
- 
-
+var titleText = [
+    "|", "$", "$ q_", "$ qb_", "$ qbe_", "$ qbek_", "$ qbeku_", "$ qbekun_", 
+    "$ qbekun_", "$ qbekun_", "$ qbekun|", "$ qbeku|", "$ qbek|", "$ qbe|", 
+    "$ qb|", "$ q|", " "
+];
 
 function loop() {
-    document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
+    document.title = titleText[x++ % titleText.length];
 }
 
 $(window).on("scroll", function () {
     var a = $("#header");
-    200 < $(window).scrollTop() ? a.addClass("sticky_navigation") : a.removeClass("sticky_navigation")
-}), $("#wrapper-dropdown").click(function (a) {
-    $(this).hasClass("active") ? $(this).removeClass("active") : $(this).addClass("active")
-}), $("a[href^='#']").click(function (a) {
+    if ($(window).scrollTop() > 200) {
+        a.addClass("sticky_navigation");
+    } else {
+        a.removeClass("sticky_navigation");
+    }
+});
+
+$("#wrapper-dropdown").click(function () {
+    $(this).toggleClass("active");
+});
+
+$("a[href^='#']").click(function (a) {
     a.preventDefault();
     var s = $(this).attr("href");
-    return $("html, body").animate({
+    $("html, body").animate({
         scrollTop: $(s).offset().top + "px"
-    }, 2e3, function () {
-        "#home" == s && ($(".navbar-auth").removeClass("advantages-a buy-a support-a").addClass("home-a"), $(".navbar-lang").removeClass("lang-advantages lang-buy lang-support").addClass("lang-home")), "#advantages" == s && ($(".navbar-auth").removeClass("home-a buy-a support-a").addClass("advantages-a"), $(".navbar-lang").removeClass("lang-home lang-buy lang-support").addClass("lang-advantages")), "#buy" == s && ($(".navbar-auth").removeClass("home-a advantages-a support-a").addClass("buy-a"), $(".navbar-lang").removeClass("lang-home lang-advantages lang-support").addClass("lang-buy")), "#support" == s && ($(".navbar-auth").removeClass("home-a advantages-a buy-a").addClass("support-a"), $(".navbar-lang").removeClass("lang-home lang-advantages lang-buy").addClass("lang-support"))
-    }), !1
-}), $(".support-item").click(function (a) {
-    $(this).is("#hw-1") && ($("#1").addClass("active-win"), $("#2").removeClass("active-win"), $("#3").removeClass("active-win"), $("#4").removeClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-2") && ($("#1").removeClass("active-win"), $("#2").addClass("active-win"), $("#3").removeClass("active-win"), $("#4").removeClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-3") && ($("#1").removeClass("active-win"), $("#2").removeClass("active-win"), $("#3").addClass("active-win"), $("#4").removeClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-4") && ($("#1").removeClass("active-win"), $("#2").removeClass("active-win"), $("#3").removeClass("active-win"), $("#4").addClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-5") && ($("#1").removeClass("active-win"), $("#2").removeClass("active-win"), $("#3").removeClass("active-win"), $("#4").removeClass("active-win"), $("#5").addClass("active-win")), $(this).is("#hw-6") && window.open("https://vk.com/qn1s_it")
-})
+    }, 2000, function () {
+        if (s === "#home") {
+            $(".navbar-auth").removeClass("advantages-a buy-a support-a").addClass("home-a");
+            $(".navbar-lang").removeClass("lang-advantages lang-buy lang-support").addClass("lang-home");
+        } else if (s === "#advantages") {
+            $(".navbar-auth").removeClass("home-a buy-a support-a").addClass("advantages-a");
+            $(".navbar-lang").removeClass("lang-home lang-buy lang-support").addClass("lang-advantages");
+        } else if (s === "#buy") {
+            $(".navbar-auth").removeClass("home-a advantages-a support-a").addClass("buy-a");
+            $(".navbar-lang").removeClass("lang-home lang-advantages lang-support").addClass("lang-buy");
+        } else if (s === "#support") {
+            $(".navbar-auth").removeClass("home-a advantages-a buy-a").addClass("support-a");
+            $(".navbar-lang").removeClass("lang-home lang-advantages lang-buy").addClass("lang-support");
+        }
+    });
+    return false;
+});
+
+$(".support-item").click(function () {
+    var activeClass = "active-win";
+    if ($(this).is("#hw-1")) {
+        $("#1").addClass(activeClass).siblings().removeClass(activeClass);
+    } else if ($(this).is("#hw-2")) {
+        $("#2").addClass(activeClass).siblings().removeClass(activeClass);
+    } else if ($(this).is("#hw-3")) {
+        $("#3").addClass(activeClass).siblings().removeClass(activeClass);
+    } else if ($(this).is("#hw-4")) {
+        $("#4").addClass(activeClass).siblings().removeClass(activeClass);
+    } else if ($(this).is("#hw-5")) {
+        $("#5").addClass(activeClass).siblings().removeClass(activeClass);
+    } else if ($(this).is("#hw-6")) {
+        window.open("https://t.me/qbekun");
+    }
+});
